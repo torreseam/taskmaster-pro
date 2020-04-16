64f4089f228422933e98bba596d292bc3e27ddf1
torreseam <615694479+torreseam@users_noreply.github.com>
2020-04-15 20:19:16 -0500

    update drop here

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var tasks = {};
 
-var createTask = function(taskText, taskDate, taskList) {
+var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
   var taskSpan = $("<span>")
@@ -18,7 +18,7 @@ var createTask = function(taskText, taskDate, taskList) {
   $("#list-" + taskList).append(taskLi);
 };
 
-var loadTasks = function() {
+var loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
@@ -32,30 +32,34 @@ var loadTasks = function() {
   }
 
   // loop over object properties
-  $.each(tasks, function(list, arr) {
-    // console.log(list, arr);
+  $.each(tasks, function (list, arr) {
     // then loop over sub-array
-    arr.forEach(function(task) {
+    arr.forEach(function (task) {
       createTask(task.text, task.date, list);
     });
   });
 };
-// save function to local storage
-var saveTasks = function() {
+
+var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// when you click the <p> in the list-group, set this as text and trim
 $(".list-group").on("click", "p", function () {
   var text = $(this)
     .text()
     .trim();
+  //textImput is the new <textarea> with calss of form-control, with a value of (text) set above
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
+  //replace this (<p> clicked) withe textInput
   $(this).replaceWith(textInput);
+  //textInput becomes the focus
+  textInput.trigger("focus");
 });
 
-// blur callback
+// blur area
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this)
@@ -73,6 +77,10 @@ $(".list-group").on("blur", "textarea", function () {
     .closest(".list-group-item")
     .index();
 
+  // return the text property of the object in the array of the given index, then save to localStorage
+  tasks[status][index].text = text;
+  saveTasks();
+
   // recreate p element
   var taskP = $("<p>")
     .addClass("m-1")
@@ -80,74 +88,73 @@ $(".list-group").on("blur", "textarea", function () {
 
   // replace textarea with p element
   $(this).replaceWith(taskP);
-
 });
 
-// due date was clicked
+// due date is clicked
 $(".list-group").on("click", "span", function () {
-  // get current text
+  //get current text
   var date = $(this)
     .text()
     .trim();
 
-  // create new input element
+  //create new input element
   var dateInput = $("<input>")
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
 
-  // swap out elements
+  //swap out elements
   $(this).replaceWith(dateInput);
 
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
-// value of due date was changed
+//value of due date was changed
 $(".list-group").on("blur", "input[type='text']", function () {
-  // get current text
+  //get current text
   var date = $(this)
     .val()
     .trim();
 
-  // get the parent ul's id attribute
+  //get the parent ul's id attribute
   var status = $(this)
     .closest(".list-group")
-    .attr("id")
+    .arrt("id")
     .replace("list-", "");
 
-  // get the task's position in the list of other li elements
+  //get the task's positon in the list of other le elements
   var index = $(this)
     .closest(".list-group-item")
     .index();
 
-  // update task in array and re-save to localstorage
-  tasks[status][index].date = date;
-  saveTasks();
+  //update task in array and re-save to localStorage
+  task[status][index].date = date;
+  taveTask();
 
-  // recreate span element with bootstrap classes
+  //reacreate span element with bootstrap classes
   var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
+    .addClass("badge badge-primary bdage-pill")
     .text(date);
 
-  // replace input with span element
+  //replace input with span element
   $(this).replaceWith(taskSpan);
 });
 
 // modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
+$("#task-form-modal").on("show.bs.modal", function () {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
 });
 
 // modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
+$("#task-form-modal").on("shown.bs.modal", function () {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-primary").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -169,7 +176,7 @@ $("#task-form-modal .btn-primary").click(function() {
 });
 
 // remove all tasks
-$("#remove-tasks").on("click", function() {
+$("#remove-tasks").on("click", function () {
   for (var key in tasks) {
     tasks[key].length = 0;
     $("#list-" + key).empty();
